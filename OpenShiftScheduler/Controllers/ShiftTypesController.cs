402545c112689de01b2c6de1ee1a0867f4f9bb25
nagasudhirpulla <nagasudhirpulla@gmail.com>
2019-06-06 00:02:11 +0530

    done

--- OpenShiftScheduler/Controllers/ShiftTypesController.cs
+++ OpenShiftScheduler/Controllers/ShiftTypesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using OpenShiftScheduler.Models.AppModels;
 
 namespace OpenShiftScheduler.Controllers
 {
+    [Authorize]
     public class ShiftTypesController : Controller
     {
         private readonly ShiftScheduleDbContext _context;
--- OpenShiftScheduler/Controllers/ShiftsApiController.cs
+++ OpenShiftScheduler/Controllers/ShiftsApiController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -14,6 +15,7 @@ namespace OpenShiftScheduler.Controllers
     [EnableCors("anyorigin")]
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class ShiftsApiController : ControllerBase
     {
         private readonly ShiftScheduleDbContext _context;
