d992fb42c283bc849d88676fd16d53f54b68a4a3
Nagasudhir Pulla <Nagasudhir@wrldc.local>
2020-08-17 10:52:12 +0530

    Fixes #28

--- src/OSS.App/ShiftParticipations/Commands/FollowShiftParticipation/FollowShiftParticipationCommandHandler.cs
+++ src/OSS.App/ShiftParticipations/Commands/FollowShiftParticipation/FollowShiftParticipationCommandHandler.cs
@@ -23,10 +23,11 @@ namespace OSS.App.ShiftParticipations.Commands.FollowShiftParticipation
         {
             // get all the shift participations to follow
             var spList = await _context.ShiftParticipations
-                                .Where(sp => sp.Shift.ShiftDate >= request.StartDate
-                                             && sp.Shift.ShiftDate <= request.EndDate
-                                             && sp.EmployeeId == request.TargetEmployeeId
-                                             && (request.TargetParticipationTypeId.HasValue && request.TargetParticipationTypeId.Value != -1) ? (sp.ShiftParticipationTypeId == request.TargetParticipationTypeId.Value) : true)
+                                .Where(sp => (sp.Shift.ShiftDate >= request.StartDate)
+                                             && (sp.Shift.ShiftDate <= request.EndDate)
+                                             && (sp.EmployeeId == request.TargetEmployeeId)
+                                             && (request.TargetParticipationTypeId == -1 ? true : (sp.ShiftParticipationTypeId == request.TargetParticipationTypeId))
+                                             )
                                 .ToListAsync();
 
             // create similar shift participations for the new employee
