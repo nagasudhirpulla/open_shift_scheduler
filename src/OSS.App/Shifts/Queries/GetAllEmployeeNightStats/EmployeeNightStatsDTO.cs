17dde60cc13efd3abeb38aecfb2322d67484d637
unknown <nagasudhirpulla@gmail.com>
2021-02-26 18:24:57 +0530

    night shift stats page done

---
+++ src/OSS.App/Shifts/Queries/GetAllEmployeeNightStats/EmployeeNightStatsDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using OSS.App.Security.Queries.GetAppUsers;
+
+namespace OSS.App.Shifts.Queries.GetAllEmployeeNightStats
+{
+    public class EmployeeNightStatsDTO
+    {
+        public UserDTO Employee { get; set; }
+        public int NumNightShiftsAllotted { get; set; } = 0;
+        public int NumNightShiftsAttended { get; set; } = 0;
+        public string NightShiftDates { get; set; }
+    }
+
+}
---
+++ src/OSS.App/Shifts/Queries/GetAllEmployeeNightStats/GetAllEmployeeNightStatsQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+
+namespace OSS.App.Shifts.Queries.GetAllEmployeeNightStats
+{
+    public partial class GetAllEmployeeNightStatsQuery : IRequest<List<EmployeeNightStatsDTO>>
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
