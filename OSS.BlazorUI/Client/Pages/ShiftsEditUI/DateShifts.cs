b77e4707465006fed5f0e54f13256bafa479d9df
unknown <nagasudhirpulla@gmail.com>
2023-10-17 17:59:40 +0530

    started shifts edit ui page with injected state

---
+++ OSS.BlazorUI/Client/Pages/ShiftsEditUI/DateShifts.cs
@@ -0,0 +1,9 @@
+﻿using OSS.Domain.Entities;
+
+namespace OSS.BlazorUI.Pages.ShiftsEditUI;
+
+public class DateShifts
+{
+    public DateOnly ShiftsDate { get; set; } = new();
+    public Dictionary<ShiftType, Shift> Shifts { get; set; } = new();
+}
