1d83b59467723432089e0493d2a804a392e5d337
Nagasudhir Pulla <Nagasudhir@wrldc.local>
2023-11-07 11:51:40 +0530

    delete shift participation feature added

---
+++ src/OSS.BlazorUI/Pages/ShiftsEditUI/Actions/DeleteShiftParticipationAction.cs
@@ -0,0 +1,41 @@
+﻿using Fluxor;
+using System.Net.Http.Json;
+using OSS.Domain.Entities;
+
+namespace OSS.BlazorUI.Pages.ShiftsEditUI.Actions;
+
+public class DeleteShiftParticipationAction
+{
+    public ShiftParticipation ShiftParticipation { get; }
+    public DeleteShiftParticipationAction(ShiftParticipation sp)
+    {
+        ShiftParticipation = sp;
+    }
+}
+
+public class DeleteShiftParticipationEffect
+{
+    private readonly HttpClient Http;
+
+    public DeleteShiftParticipationEffect(HttpClient http) => Http = http;
+
+    [EffectMethod]
+    public async Task DeleteShiftParticipation(CreateShiftParticipationAction action, IDispatcher dispatcher)
+    {
+        var shiftParticipation = action.ShiftParticipation;
+        HttpResponseMessage resp = await Http.DeleteAsync($"api/ShiftParticipations/{shiftParticipation.Id}");
+        if (!resp.IsSuccessStatusCode)
+        {
+            Console.WriteLine("Error in deleting a shift participation at server");
+            return;
+        }
+        ShiftParticipation? deletedShiftPart = await resp.Content.ReadFromJsonAsync<ShiftParticipation>();
+        if (deletedShiftPart == null)
+        {
+            Console.WriteLine("Unable to parse deleted shift object returned from server");
+            return;
+        }
+        // delete shift participation from UI
+        dispatcher.Dispatch(new DeleteShiftParticipationFromUiAction(deletedShiftPart));
+    }
+}
---
+++ src/OSS.BlazorUI/Pages/ShiftsEditUI/Actions/DeleteShiftParticipationFromUiAction.cs
@@ -0,0 +1,47 @@
+﻿using Fluxor;
+using OSS.BlazorUI.Pages.ShiftsEditUI.Store;
+using OSS.Domain.Entities;
+
+namespace OSS.BlazorUI.Pages.ShiftsEditUI.Actions;
+
+public class DeleteShiftParticipationFromUiAction
+{
+    public ShiftParticipation ShiftParticipation { get; }
+    public DeleteShiftParticipationFromUiAction(ShiftParticipation shiftParticipation) => ShiftParticipation = shiftParticipation;
+
+    [ReducerMethod]
+    public static ShiftsEditUiState OnDeleteShiftParticipationFromUi(ShiftsEditUiState state, DeleteShiftParticipationFromUiAction action)
+    {
+        int shiftInd = -1;
+        int shiftPartInd = -1;
+
+        // find the shift and shift participation that contains this participation
+        for (int sIter = 0; sIter < state.Shifts.Count; sIter++)
+        {
+            var shift = state.Shifts[sIter];
+            for (int spIter = 0; spIter < shift.ShiftParticipations.Count; spIter++)
+            {
+                var sp = shift.ShiftParticipations[spIter];
+                if (sp.Id == action.ShiftParticipation.Id)
+                {
+                    shiftInd = sIter;
+                    shiftPartInd = spIter;
+                    break;
+                }
+            }
+            if (shiftInd != -1) break;
+        }
+
+        if (shiftInd == -1)
+        {
+            Console.WriteLine($"Shift participation not found in UI for deletion");
+            return state;
+        }
+
+        var shifts = state.Shifts;
+        shifts[shiftInd].ShiftParticipations.RemoveAt(shiftPartInd);
+
+        return SetShiftsAction.OnSetShifts(state, new SetShiftsAction(shifts));
+        // TODO test this action
+    }
+}
