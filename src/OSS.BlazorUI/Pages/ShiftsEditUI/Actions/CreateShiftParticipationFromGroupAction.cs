18190577b73d24f059ce0995e420839825b38b74
Nagasudhir Pulla <Nagasudhir@wrldc.local>
2023-11-10 11:16:57 +0530

    create participations from group feature added

---
+++ src/OSS.BlazorUI/Pages/ShiftsEditUI/Actions/CreateShiftParticipationFromGroupAction.cs
@@ -0,0 +1,46 @@
+﻿using Fluxor;
+using OSS.Domain.Entities;
+using System.Net.Http.Json;
+
+namespace OSS.BlazorUI.Pages.ShiftsEditUI.Actions;
+
+public class CreateShiftParticipationFromGroupAction
+{
+    public int ShiftId { get; }
+    public int ShiftGroupId { get; }
+    public CreateShiftParticipationFromGroupAction(int sId, int sgId)
+    {
+        ShiftId = sId;
+        ShiftGroupId = sgId;
+    }
+}
+
+public class CreateShiftParticipationFromGroupEffect
+{
+    private readonly HttpClient Http;
+
+    public CreateShiftParticipationFromGroupEffect(HttpClient http) => Http = http;
+
+    [EffectMethod]
+    public async Task CreateShiftParticipationFromGroup(CreateShiftParticipationFromGroupAction action, IDispatcher dispatcher)
+    {
+        HttpResponseMessage resp = await Http.PostAsJsonAsync($"api/ShiftParticipations/FromGroup", action);
+        if (!resp.IsSuccessStatusCode)
+        {
+            Console.WriteLine("Error in creating shift participations from shift group at server");
+            return;
+        }
+        List<ShiftParticipation>? createdShiftParticipations = await resp.Content.ReadFromJsonAsync<List<ShiftParticipation>>();
+        if (createdShiftParticipations == null)
+        {
+            Console.WriteLine("Unable to parse created shift participations list returned from server");
+            return;
+        }
+
+        foreach (var sp in createdShiftParticipations)
+        {
+            dispatcher.Dispatch(new AddShiftParticipationToUiAction(sp));
+        }
+        // TODO test this
+    }
+}
