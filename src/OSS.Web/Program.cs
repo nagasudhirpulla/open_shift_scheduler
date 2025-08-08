1f3a8f20dc884b912d22f2e31ddca1b088f6c7fb
Nagasudhir <Nagasudhir@SUDHIR>
2025-08-08 14:29:43 +0530

    npgsql fix

--- src/OSS.Web/Program.cs
+++ src/OSS.Web/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace OSS.Web;
 
@@ -7,6 +8,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
         CreateHostBuilder(args).Build().Run();
     }
 
